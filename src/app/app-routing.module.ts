dd6288aa9563543524175a8bce12ea905de2e2ef
AzeemJokosenumi <62096498+AzeemJokosenumi@users.noreply.github.com>
2021-08-21 02:14:12 +0100

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'ulster',
+    loadChildren: () => import('./ulster/ulster.module').then( m => m.UlsterPageModule)
+  },
+  {
+    path: 'munster',
+    loadChildren: () => import('./munster/munster.module').then( m => m.MunsterPageModule)
+  },
+  {
+    path: 'connaught',
+    loadChildren: () => import('./connaught/connaught.module').then( m => m.ConnaughtPageModule)
+  },
+  {
+    path: 'connaughtcountries',
+    loadChildren: () => import('./connaughtcountries/connaughtcountries.module').then( m => m.ConnaughtcountriesPageModule)
+  },
+  {
+    path: 'leinster',
+    loadChildren: () => import('./leinster/leinster.module').then( m => m.LeinsterPageModule)
+  },
+  {
+    path: 'leinstercountries',
+    loadChildren: () => import('./leinstercountries/leinstercountries.module').then( m => m.LeinstercountriesPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
